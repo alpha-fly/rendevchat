827eb985df08f18263e9f1b3f62d2f9bbc6fc521
alpha-fly <placetobe@naver.com>
2022-07-12 10:03:37 +0900

    1st commit : ejs 1st application

---
+++ src/public/js/app.js
@@ -0,0 +1,242 @@
+const socket = io();
+
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+const finishBtn = document.getElementById("finish"); // 인터뷰 종료버튼
+const camerasSelect = document.getElementById("cameras");
+
+const call  = document.getElementById("call")
+const chat  = document.getElementById("chat")
+
+call.hidden = true;
+chat.hidden = true;
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+let roomName;
+let myPeerConnection;
+
+// 사용자의 카메라 장치(mediaDevices) 불러오기
+async function getCameras() {
+    try {
+        const devices = await navigator.mediaDevices.enumerateDevices();        
+        const cameras = devices.filter(device => device.kind === "videoinput");
+        const currentCamera = myStream.getVideoTracks()[0];
+        cameras.forEach(camera => {
+            const option = document.createElement("option");
+            option.value = camera.deviceId
+            option.innerText = camera.label
+            if (currentCamera.label == camera.label) {
+                option.selected = true;
+            }
+            camerasSelect.appendChild(option)
+        })        
+    } catch (e) {
+        console.log (e)
+    }
+}
+
+// 사용자의 비디오 입력(media) 받아서 myStream 생성하기
+async function getMedia(deviceId) {
+    const initialConstraints = {
+        audio : true,
+        video : { facingMode : "user"}, 
+    };
+
+    const cameraConstraints = {
+        audio : true, 
+        video : {deviceId: { exact : deviceId}},
+    }
+    try {
+        myStream = await navigator.mediaDevices.getUserMedia(
+            deviceId? cameraConstraints : initialConstraints
+            );            
+            myFace.srcObject = myStream;
+            if (!deviceId) {
+                await getCameras();
+            }
+    } catch(e){
+        console.log(e)
+    }
+}
+
+// Mute 버튼 작동
+function handleMuteClick () {    
+    myStream.getAudioTracks().forEach((track) => (track.enabled = !track.enabled));
+    if (!muted) {
+        muteBtn.innerText = "내 마이크 끄기"        
+        muted = true;
+    } else {
+        muteBtn.innerText = "내 마이크 켜기"        
+        muted = false;
+    }
+}
+
+// 카메라 on/off 작동
+function handleCameraClick () {    
+    myStream.getVideoTracks().forEach((track) => (track.enabled = !track.enabled));
+    if (cameraOff) {
+        cameraBtn.innerText = "내 카메라 끄기"        
+        cameraOff = false
+    } else {
+        cameraBtn.innerText = "내 카메라 켜기"        
+        cameraOff = true
+    }
+}
+
+async function handleCameraChange() {
+    await getMedia(camerasSelect.value)
+    if (myPeerConnection) {
+        const videoTrack = myStream.getVideoTracks()[0];
+        const videoSender = myPeerConnection.getSenders().find((sender) => sender.track.kind === "video");
+        videoSender.replaceTrack(videoTrack);
+    }
+}
+
+// 인터뷰 종료 버튼 클릭시 : Video, Audio 끄고 종료 버튼 감추고 alert 띄워준다.
+async function handleFinishInterview() {  
+    finishBtn.hidden = true;
+    myStream.getAudioTracks().forEach((track) => (track.enabled = false));
+    myStream.getVideoTracks().forEach((track) => (track.enabled = false));
+    alert("인터뷰 완료를 확인했습니다. 좋은 결과 기다릴게요 :)")
+    socket.emit("finish_interview", roomName);  
+}
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
+finishBtn.addEventListener("click", handleFinishInterview) // 인터뷰 종료 버튼에 이벤트 배정
+camerasSelect.addEventListener("input", handleCameraChange);
+
+
+// welcome Form (choose a room)
+const welcome = document.getElementById("welcome")
+const welcomeForm = welcome.querySelector("form");
+
+async function initCall() {
+    welcome.hidden = true; 
+    call.hidden = false;
+    chat.hidden = false;
+
+    const msgForm = chat.querySelector("#msg");
+    msgForm.addEventListener("submit", handleMessageSubmit)  
+
+    await getMedia();
+    makeConnection();
+};
+
+// (2) interview code(room name)을 입력받고 join_room에 전달한다. 
+async function handleWelcomeSubmit(event) {
+    event.preventDefault();
+    const input = welcomeForm.querySelector("input");
+    socket.emit("check_code", input.value );  
+    roomName = input.value
+    input.value=""
+}
+
+socket.on("right_code", async (roomName) => {
+    await initCall();
+    socket.emit("join_room", roomName );                
+});
+
+socket.on("wrong_code", async (errormessage) => {
+    alert(errormessage);
+});
+
+// 방 입장하기
+welcomeForm.addEventListener("submit", handleWelcomeSubmit)
+
+//socket code part 1 : 영상채팅용 socket 통신 (WebRTC peer-to-peer 연결을 위한 부분)
+
+// (4) 방에 입장되었다. 시그널링을 시작함, offer의 내용을 만들어 서버에 보낸다. 
+socket.on("welcome", async () => {
+    const offer = await myPeerConnection.createOffer();    
+    myPeerConnection.setLocalDescription(offer)
+    console.log("sent the offer")
+    socket.emit("offer", offer, roomName);
+});
+
+// (6) offer를 받았다. answer의 내용을 만들어 서버에 보낸다. 
+socket.on("offer", async (offer) => {
+    console.log("received the offer")
+    myPeerConnection.setRemoteDescription(offer);
+    const answer = await myPeerConnection.createAnswer();
+    myPeerConnection.setLocalDescription(answer);
+    socket.emit("answer", answer, roomName);
+    console.log("sent the answer")
+});
+
+// (8) answer까지 받았다. remote description 을 생성한다. 
+socket.on("answer", (answer) => {
+    console.log("received the answer")
+    myPeerConnection.setRemoteDescription(answer);
+});
+
+socket.on("ice", ice => {
+    console.log("received candidate")
+    myPeerConnection.addIceCandidate(ice);
+})
+
+//socket code part 2 : 텍스트 채팅 핸들링
+function addMessage(message) {
+    const ul = chat.querySelector("ul")
+    const li = document.createElement("li")
+    li.innerText = message;
+    ul.appendChild(li);
+}
+
+async function handleMessageSubmit(event) {
+    event.preventDefault();
+    const input = chat.querySelector("#msg input");
+    const value = input.value
+   
+    socket.emit("new_message", input.value, roomName, () => {
+        addMessage(`You : ${value}`);    
+    });
+    input.value=""
+}
+
+socket.on("new_message", (msg) => {addMessage(msg)});
+
+// socket.on("bye", (left, newCount) => {
+//     const h3 = room.querySelector("h3");
+//     h3.innerText = `Room ${roomName} (${newCount})`;
+//     addMessage(`${left} left...`)
+// })
+
+
+// WebRTC Code
+function makeConnection () {
+    myPeerConnection = new RTCPeerConnection({
+
+        iceServers: [
+            { urls: 'stun:stun.l.google.com:19302' },
+            { 
+                username: 'alphafly',
+                credential: '1324',
+                urls: [
+                    'turn:3.34.42.87:3478?transport=tcp',
+                    'turn:3.34.42.87:3478?transport=udp',                    
+                    'turn:3.34.42.87:80?transport=tcp',
+                    'turn:3.34.42.87:80?transport=udp',
+                    'turns:3.34.42.87:443?transport=tcp',
+                    'turns:3.34.42.87:5349?transport=tcp',
+                    ] 
+            }
+        ]
+    });
+    myPeerConnection.addEventListener("icecandidate", handleIce)
+    myPeerConnection.addEventListener("addstream", handleAddStream)
+    myStream.getTracks().forEach(track => myPeerConnection.addTrack(track, myStream));
+}
+
+function handleIce (data) {
+    console.log("sent candidate")
+    socket.emit("ice", data.candidate, roomName)    
+}
+
+function handleAddStream(data) {
+    const peerFace = document.getElementById("peerFace");    
+    peerFace.srcObject = data.stream    
+}
---
+++ src/server.js
@@ -0,0 +1,200 @@
+import http from "http";
+import { Server } from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
+import express from "express";
+import mysql from "mysql2";  
+import ejs from "ejs";
+import path from "path";
+require("dotenv").config();
+
+const app = express();
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("index"));
+app.get("/*", (req, res) => res.redirect("/"));
+
+const httpServer = http.createServer(app);
+
+// 502 Bad gateway 에러에 대한 대응: Idle timeout 값을 크게 잡아준다. (이것 만으로는 해결 안됐음)
+// AWS ALB의 기본 Idle timeout 값은 60초. 아래와 같이 65, 66초로 잡아줌.
+httpServer.keepAliveTimeout = 65000;
+httpServer.headersTimeout = 66000;
+
+const wsServer = new Server(httpServer, {
+  cors: {
+    origin: ["https://admin.socket.io"],
+    credentials: true,
+  },
+});
+instrument(wsServer, {
+  auth: false,
+});
+
+// MySQL DB에 연결 (Pool)
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: "root",
+  password: process.env.DB_PASSWORD,
+  database: "rendevSQL",  
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
+wsServer.on("connection", (socket) => {
+  // (1) 소켓 접속되면 일단 nickname 을 Anonymous로 디폴트 설정해줌
+  socket["nickname"] = "Anonymous";
+
+  // 모든 socket 이벤트에 대한 log 표시 (필요시에만 주석을 풀어 사용할 것)
+  // socket.onAny((event) => {
+  //     console.log(wsServer.sockets.adapter);
+  //     console.log(`Socket Event : ${event}`)
+  // });
+
+  // (3-1) 입력된 interview code가 옳은지 검증한다 (DB의 application 테이블에 등록된 코드인가?)
+  socket.on("check_code", (code) => {
+   
+    pool.getConnection(function(err, conn){
+      conn.query(
+        "SELECT interviewCode, schedule, status from application",
+        (error, results) => {
+          if (error) {
+            console.log(error);
+            pool.releaseConnection(conn);
+          }
+
+          // 입력받은 면접코드가 DB의 application 테이블의 interviewCode 칼럼에 등록된 코드인지 확인한다.
+          const interview = results.find(
+            (application) => application["interviewCode"] === code
+          );
+          
+          if (!interview) {
+            const errormessage = "인터뷰 코드가 바르지 않습니다.";
+            socket.emit("wrong_code", errormessage);
+            pool.releaseConnection(conn);
+            return;
+          }          
+          console.log(interview);
+
+          // 현재 시각이 인터뷰 예약시간을 기준으로 "15분전 ~ 3시간 후" 사이일 때만 인터뷰 입장이 가능하다.        
+          const currentTime = new Date();          
+          const fifteenMinEarlier = new Date( (Date.parse(interview["schedule"])) - 1000 * 60 * 15 );
+          const threeHrslater = new Date( (Date.parse(interview["schedule"])) + 1000 * 60 * 60 * 3 );
+
+          console.log("현재시각 :", currentTime)
+          console.log("인터뷰시간:", interview["schedule"])
+          console.log("15분 전  :", fifteenMinEarlier)
+          console.log("3시간 후 :", threeHrslater)        
+
+          if (
+            currentTime < fifteenMinEarlier ||
+            currentTime > threeHrslater
+          ) {
+            const errormessage =
+              `인터뷰 예약시간 기준 "15분 전 ~ 3시간 후" 사이에만 입장 가능합니다.`;
+
+            socket.emit("wrong_code", errormessage);
+            pool.releaseConnection(conn);
+            return;
+          }
+
+          // 위 두 조건을 모두 통과했다면 영상통화방으로 입장한다.
+          socket.emit("right_code", code);
+          // pool.releaseConnection(conn);
+      })  
+    });   
+  });
+
+  // (3-2) 전달받은 room name 으로 입장한다 (없는 경우 room 만들면서 입장)
+  socket.on("join_room", (roomName) => {
+    socket.join(roomName);
+    socket.to(roomName).emit("welcome");
+  });
+
+  // 아래 offer, answer, ice : WebRTC peer-to-peer 연결을 위해 socket으로 시그널링
+
+  // (5) offer 내용을 전달받고 같은 방에 offer를 보낸다.
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer);
+  });
+
+  // (7) answer의 내용을 전달받아 같은 방에 answer를 보낸다.
+  socket.on("answer", (answer, roomName) => {
+    socket.to(roomName).emit("answer", answer);
+  });
+
+  socket.on("ice", (ice, roomName) => {
+    socket.to(roomName).emit("ice", ice);
+  });
+
+  // 이하 text chat을 위한 socket 통신
+  socket.on("new_message", (msg, roomName, done) => {
+    socket.to(roomName).emit("new_message", `${socket.nickname}: ${msg}`);
+    done();
+  });
+
+  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
+
+  // 인터뷰 종료 버튼을 눌렀을 때 DB에 상태변화 적용해주기
+  socket.on("finish_interview", (roomName) => {
+    pool.getConnection(function(err, conn){
+      pool.query(
+        `SELECT interviewCode, status 
+              from application
+              WHERE interviewCode="${roomName}"`,
+        (error, results) => {
+          if (error) {
+            console.log(error);
+            pool.releaseConnection(conn);
+          }
+          console.log(results);
+
+          // status가 finish(1)이나 finish(2)가 아닐 경우, finish(1)로 바꿔준다.
+          if (
+            results[0]["status"] !== "finish(1)" &&
+            results[0]["status"] !== "finish(2)"
+          ) {
+            pool.query(
+              `UPDATE application 
+                        SET status="finish(1)" 
+                        WHERE interviewCode="${roomName}"`,
+              (error, results) => {
+                if (error) {
+                  console.log(error);
+                  pool.releaseConnection(conn);
+                }
+              }
+            );
+
+          // status가 finish(1)이라면, finish(2)로 바꿔준다.
+          } else if (results[0]["status"] == "finish(1)") {
+            pool.query(
+              `UPDATE application 
+                        SET status="finish(2)" 
+                        WHERE interviewCode="${roomName}"`,
+              (error, results) => {
+                if (error) {
+                  console.log(error);
+                  pool.releaseConnection(conn);
+                }
+              });
+          }
+        }
+      )
+      pool.releaseConnection(conn);
+    });
+  });  
+
+  // socket.on("disconnecting", () => {
+  //     socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname));
+  // });
+
+  // socket.on("disconnect", () => {
+  //     wsServer.sockets.emit("room_change", publicRooms());
+  // })
+});
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
