7297578345c0e72a87d8dcb34791194514e0d94b
alpha-fly <placetobe@naver.com>
2022-07-29 09:24:51 +0900

    FIX: 현재 배포 기준 DB(RDS)에 코드검증 연결

--- src/server.js
+++ src/server.js
@@ -37,7 +37,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: "root",
   password: process.env.DB_PASSWORD,
-  database: "rendevSQL",  
+  database: "rendevSQLtest",  
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
